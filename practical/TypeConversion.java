30d71360db524fff7b9c6aa8893f163d8e60316c
Akhilesh52 <akhileshvukyam@gmail.com>
2025-07-26 10:44:58 +0530

    practical

---
+++ practical/TypeConversion.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+        double myDouble = 9.78;
+        int myInt = (int) myDouble;
+        System.out.println("Original double: " + myDouble);
+        System.out.println("Converted int: " + myInt);
+    }
+}
