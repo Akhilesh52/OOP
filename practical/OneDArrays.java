ad86b3092642409af17d53eee014ec02f140b6ac
Akhilesh52 <akhileshvukyam@gmail.com>
2025-07-26 11:04:28 +0530

    Update OneDArrays.java

--- practical/OneDArrays.java
+++ practical/OneDArrays.java
@@ -5,7 +5,7 @@ public class OneDArrays
 
     public static void main(String[] args) 
     {
-        int[] marks = {85, 90, 75, 88};
+        int[] marks = {85, 91, 76, 89};
 
         for (int i = 0; i < marks.length; i++) 
         {
