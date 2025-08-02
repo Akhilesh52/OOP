111ca8caee5d44fc81a0c39cfb3cbbe4c7301ca7
Akhilesh52 <akhileshvukyam@gmail.com>
2025-08-02 11:17:05 +0530

    Add files via upload

---
+++ practical/StringBuilders.java
@@ -0,0 +1,12 @@
+package Skill;
+
+public class StringBuilders {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb = new StringBuilder("Hello ");
+		sb.insert(6,"Java");
+		System.out.println(sb);
+		}
+	}
+
